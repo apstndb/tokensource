51412402829561ca74cc6bb3ac9bebb96d3ac87a
apstndb <803393+apstndb@users.noreply.github.com>
2021-05-23 02:23:15 +0900

    Use backoff package

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/apstndb/tokensource
 go 1.16
 
 require (
-	github.com/lthibault/jitterbug/v2 v2.2.2
+	github.com/cenkalti/backoff/v4 v4.1.0
 	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c
 	google.golang.org/api v0.47.0
 )
--- refresh.go
+++ refresh.go
@@ -3,10 +3,11 @@ package tokensource
 import (
 	"context"
 	"log"
+	"math/rand"
 	"sync"
 	"time"
 
-	"github.com/lthibault/jitterbug/v2"
+	"github.com/cenkalti/backoff/v4"
 	"golang.org/x/oauth2"
 )
 
@@ -27,17 +28,22 @@ func (ts *AsyncRefreshingTokenSource) Token() (*oauth2.Token, error) {
 
 // AsyncRefreshingConfig is the refresh configuration of NewAsyncRefreshingTokenSource.
 type AsyncRefreshingConfig struct {
-	// BeforeExpiryMargin A is the margin for refreshing the token before Expiry.
+	// MarginBeforeExpiry is the margin for refreshing the token before Expiry.
 	// If it is zero value, TokenSource don't care about Expiry.
-	BeforeExpiryMargin time.Duration
+	MarginBeforeExpiry time.Duration
+	// RandomizationFactorForRefreshInterval is randomization factor for MarginBeforeExpiry.
+	RandomizationFactorForMarginBeforeExpiry float64
+
 	// RefreshInterval is interval for refreshing token if Expiry based refreshing is not applied.
 	// If not set, 10 minutes is default interval.
 	RefreshInterval time.Duration
-	// JitterFunc is jitter function for RefreshInterval and BeforeExpiryMargin.
-	// It is compatible with https://pkg.go.dev/github.com/lthibault/jitterbug#Jitter.
-	JitterFunc interface {
-		Jitter(time.Duration) time.Duration
-	}
+	// RandomizationFactorForRefreshInterval is randomization factor for RefreshInterval.
+	RandomizationFactorForRefreshInterval float64
+
+	// Backoff is backoff configuration for TokenSource.Token().
+	// If not set, backoff.NewExponentialBackOff is used as the default value.
+	// See also https://pkg.go.dev/github.com/cenkalti/backoff/v4#NewExponentialBackOff.
+	Backoff backoff.BackOff
 }
 
 // NewAsyncRefreshingTokenSource create TokenSource with the refresh config conf and the TokenSource generator function genFunc.
@@ -47,8 +53,8 @@ func NewAsyncRefreshingTokenSource(ctx context.Context, conf AsyncRefreshingConf
 	if conf.RefreshInterval == 0 {
 		conf.RefreshInterval = defaultInterval
 	}
-	if conf.JitterFunc == nil {
-		conf.JitterFunc = jitterbug.Norm{}
+	if conf.Backoff == nil {
+		conf.Backoff = backoff.NewExponentialBackOff()
 	}
 	b := &AsyncRefreshingTokenSource{genFunc: genFunc, conf: conf}
 	expiry, err := b.flip(ctx)
@@ -60,28 +66,41 @@ func NewAsyncRefreshingTokenSource(ctx context.Context, conf AsyncRefreshingConf
 }
 
 func (ts *AsyncRefreshingTokenSource) flip(ctx context.Context) (time.Time, error) {
-	tokenSource, err := ts.genFunc(ctx)
-	if err != nil {
-		return time.Time{}, err
-	}
-	t, err := tokenSource.Token()
+	var token *oauth2.Token
+	err := backoff.Retry(func() error {
+		tokenSource, err := ts.genFunc(ctx)
+		if err != nil {
+			return err
+		}
+
+		t, err := tokenSource.Token()
+		if err != nil {
+			return err
+		}
+		token = t
+		return nil
+	}, ts.conf.Backoff)
 	if err != nil {
 		return time.Time{}, err
 	}
 
 	ts.mu.Lock()
-	ts.token = t
+	ts.token = token
 	ts.mu.Unlock()
-	return t.Expiry, nil
+	return token.Expiry, nil
 }
 
 func (ts *AsyncRefreshingTokenSource) run(ctx context.Context, initialExpiry time.Time) {
-	ticker := jitterbug.New(ts.conf.RefreshInterval, ts.conf.JitterFunc)
+	ticker := tickerWithJitter(ts.conf.RefreshInterval, ts.conf.RandomizationFactorForRefreshInterval)
 	defer ticker.Stop()
 
 	handleExpiry := func(expiry time.Time) <-chan time.Time {
-		if ts.conf.BeforeExpiryMargin != 0 && !expiry.IsZero() {
-			targetTime := expiry.Add(-ts.conf.JitterFunc.Jitter(ts.conf.BeforeExpiryMargin))
+		if ts.conf.MarginBeforeExpiry != 0 && !expiry.IsZero() {
+			delta := time.Duration(ts.conf.RandomizationFactorForMarginBeforeExpiry * float64(ts.conf.MarginBeforeExpiry))
+			// [-1.0,1.0)
+			plusMinus1 := 2 * (rand.Float64() - 0.5)
+			jitter := time.Duration(plusMinus1 * float64(delta))
+			targetTime := expiry.Add(-ts.conf.MarginBeforeExpiry + jitter)
 			return time.After(time.Until(targetTime))
 		} else {
 			return nil
@@ -110,4 +129,19 @@ loop:
 		}
 		waitUntilExpiryC = handleExpiry(expiry)
 	}
+}
+
+func tickerWithJitter(d time.Duration, randomizationFactor float64) *backoff.Ticker {
+	// (Implementation detail) It use backoff package to reduce dependency.
+	backoffForJitteredTicker := &backoff.ExponentialBackOff{
+		InitialInterval:     d,
+		RandomizationFactor: randomizationFactor,
+		Multiplier:          1.0,
+		MaxInterval:         d,
+		MaxElapsedTime:      0,
+		Clock:               backoff.SystemClock,
+	}
+	backoffForJitteredTicker.Reset()
+
+	return backoff.NewTicker(backoffForJitteredTicker)
 }
